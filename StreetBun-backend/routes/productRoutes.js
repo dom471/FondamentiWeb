6b571aa47f2f5c9ef426c86428dbf6871956f343
stefanotroilo <stef.troilo2003@gmail.com>
2026-01-12 17:53:07 +0100

    Update productRoutes.js

--- StreetBun-backend/routes/productRoutes.js
+++ StreetBun-backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
+// Route per la gestione dei prodotti
 import express from "express";
 import Product from "../models/Product.js";
 
 const router = express.Router();
 
-// GET tutti i prodotti
-router.get("/", async (req, res) => {
+// GET /api/products, recupera tutti i prodotti
+router.get("/", async (_req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(500).json({ error: "Errore nel recupero prodotti" });
   }
 });
@@ -20,19 +22,9 @@ router.post("/", async (req, res) => {
     const newProduct = new Product({ name, price, image, description });
     const saved = await newProduct.save();
     res.status(201).json(saved);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(400).json({ error: "Errore nella creazione del prodotto" });
-  }
-});
-
-// PUT /api/products/:id, posso  modificare un prodotto
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updated = await Product.findByIdAndUpdate(id, req.body, { new: true });
-    res.json(updated);
-  } catch (err) {
-    res.status(400).json({ error: "Errore nella modifica del prodotto" });
   }
 });
 
@@ -42,10 +34,10 @@ router.delete("/:id", async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.json({ message: "Prodotto eliminato" });
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(400).json({ error: "Errore nell'eliminazione del prodotto" });
   }
 });
 
-
-export default router;
+export default router; 
