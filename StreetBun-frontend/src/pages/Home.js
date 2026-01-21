4824b7b274b096b598358fa8a27266ee7981128b
stefanotroilo <stef.troilo2003@gmail.com>
2026-01-21 10:49:03 +0100

    Update Home.js

--- StreetBun-frontend/src/pages/Home.js
+++ StreetBun-frontend/src/pages/Home.js
@@ -91,7 +91,7 @@ function Home() {
 
         <Grid container spacing={4} justifyContent="center">
           {products.map((item) => (
-            <Grid item key={item.name} xs={12} sm={6} md={3}>
+            <Grid item key={item.name}>
               <Card sx={{height: "100%", display: "flex", flexDirection: "column",transition: "transform 0.2s", "&:hover": { transform: "scale(1.05)" }, borderRadius: "16px", boxShadow: 3, }}>
                 <CardMedia component="img" height="200" image={item.image} alt={item.name}/>
                 <CardContent>
